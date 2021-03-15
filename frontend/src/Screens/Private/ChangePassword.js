be40576ce8dbec3b8f36fe0018f26ee1bf8a2e06
mcc <60859013+AlexisBal@users.noreply.github.com>
2021-03-15 13:41:03 +0100

    css

--- frontend/src/Screens/Private/ChangePassword.js
+++ frontend/src/Screens/Private/ChangePassword.js
@@ -8,11 +8,11 @@ function ChangePassword () {
   const [newPasswordConfirm, setNewPasswordConfirm] = useState("");
     
   return (
-    <body className="change-password">
+    <body>
       <div className='safe-container'>
-        <h1>Modification du mot de passe</h1>
-        <main className="form-settings">
-          <form className="text-center">
+        <main className="form-change-password text-center">
+          <h1 className="h3 mb-3 fw-normal">Modification du mot de passe</h1>
+          <form>
             <input type="password" id="password" className="form-control" placeholder="Mot de passe actuel" required autoFocus onChange={e => setPassword(e.target.value)}></input>
             <input type="password" id="new-password" className="form-control" placeholder="Nouveau mot de passe" required onChange={e => setNewPassword(e.target.value)}></input>
             <input type="password" id="new-password-confirm" className="form-control" placeholder="Confirmation du mot de passe" required onChange={e => setNewPasswordConfirm(e.target.value)}></input>
